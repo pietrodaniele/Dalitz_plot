d0083dd785c8b2258189849c623d999ca3623e21
pietrodaniele <pietro.daniele@studenti.unimi.it>
2021-11-16 16:12:57 +0100

    Added S_ij class

---
+++ s_ij.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include "funzioni.h"
+#include "random.h"
+#include "s_ij.h"
+
+using namespace std;
+
+S_ij :: S_ij(){}
+
+S_ij :: ~S_ij(){}
---
+++ s_ij.h
@@ -0,0 +1,27 @@
+#ifndef __S_ij_
+#define __S_ij__
+
+#include <iostream>
+#include <vector>
+#include "random.h"
+#include "funzioni.h"
+
+using namespace std;
+
+class S_ij {
+    // using s23 as reference value
+    private:
+	    double s12, s23, s31;
+    protected:
+
+    public:
+        // constructors
+        S_ij();
+        // destructor
+        ~S_ij();
+        // setting
+        
+        // methods
+  	
+
+#endif // __S_ij__
